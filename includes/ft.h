bbedc76c5d00a6d60f6d29b29cb0b2df6facb916
lfarias- <leofariasrj25@gmail.com>
2022-04-22 06:04:13 +0200

    Updated ft.h with ft_strlen

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 05:07:52 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/04/22 05:09:38 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/04/22 05:54:01 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@
 #include <unistd.h>
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
+int		ft_strlen(char *str);
 #endif
